f0857132e7be92c7614e30a02c7bc7adb48c0faf
Fooyer <xneerd157@gmail.com>
2025-06-23 21:10:44 -0300

    alt

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Ementa from "./pages/Ementa";
 import Apresentacoes from "./pages/Apresentacoes";
 import Podcasts from "./pages/Podcasts";
 import Cases from "./pages/Cases";
-import Cases1 from "./pages/Cases/1";
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
           <Route path="/apresentacoes" element={<Apresentacoes />} />
           <Route path="/podcasts" element={<Podcasts />} />
           <Route path="/cases" element={<Cases />} />
-          <Route path="/cases/ia-na-tuper" element={<Cases1 />} />
         </Routes>
       </Layout>
     </Router>
